322ff76fe1d76c46656bd2d86a7c122d6ef6cb10
Clemens Pilz <office@clemenspilz.com>
2023-01-30 21:19:32 +0100

    Added a function that will store the resulttime as JSON in localstorage and display it in the next winscreen

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -39,10 +39,28 @@ export default async function setCarousel() {
         let extraSeconds = seconds % 60;
         minutes = minutes < 10 ? "0" + minutes : minutes;
         extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds;
-        console.log(typeof extraSeconds);
-        extraSeconds = extraSeconds.substring(0, 2);
+        extraSeconds = extraSeconds.toString().substring(0, 2);
         resultDiv.innerHTML = 'It took you ' + minutes + ' Minutes and ' + extraSeconds + ' Seconds to complete the Challenge!';
         document.body.appendChild(resultDiv);
+        let resultObj = {
+            minutes: minutes,
+            seconds: extraSeconds
+        };
+        checkStorage(resultObj);
+    }
+
+    //Checks sessionstorage for the results of the last playthrough
+    function checkStorage(obj) {
+        if (sessionStorage.getItem("result")) {
+            let prevObj = sessionStorage.getItem("result");
+            prevObj = JSON.parse(prevObj);
+            let prevDiv = document.createElement("div");
+            let prevString = document.createTextNode("Your previous result was: "
+                + prevObj.minutes + " minutes, " + prevObj.seconds + " seconds!");
+            prevDiv.appendChild(prevString);
+            document.body.appendChild(prevDiv);
+        }
+        sessionStorage.setItem("result", JSON.stringify(obj));
     }
 
     function go() {
--- scripts/winCount.js
+++ scripts/winCount.js
@@ -1,23 +1,4 @@
 //Need global variable to keep track of wins
 let winCount = 0;
+//Need global variable for timestamp
 let startTime;
-
-//Use this cheat via console if you need some cat-content to cheer you up
-function cat() {
-    document.body.innerHTML = "";
-    document.body.style.boxShadow = "0 0 0 white";
-    let img = document.createElement("img");
-    img.src = "./images/cat.gif";
-    document.body.appendChild(img);
-    document.body.style.backgroundColor = "white";
-    let resultDiv = document.createElement("div");
-    let seconds = (Date.now() - startTime) / 1000;
-    let minutes = Math.floor(seconds / 60);
-    let extraSeconds = seconds % 60;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds;
-    console.log(typeof extraSeconds);
-    extraSeconds = extraSeconds.substring(0, 2);
-    resultDiv.innerHTML = 'It took you ' + minutes + ' Minutes and ' + extraSeconds + ' Seconds to complete the Challenge!';
-    document.body.appendChild(resultDiv);
-}
