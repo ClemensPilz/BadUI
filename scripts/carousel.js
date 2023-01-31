1bca82afc8a6a607873151a08b0db64e6c7f8b3a
Clemens Pilz <office@clemenspilz.com>
2023-01-31 12:58:40 +0100

    Fine-tuned the rounding of the extraSeconds in catTime()

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -29,17 +29,17 @@ export default async function setCarousel() {
     function catTime() {
         document.body.innerHTML = "";
         document.body.style.boxShadow = "0 0 0 white";
+        document.body.style.backgroundColor = "white";
         let img = document.createElement("img");
         img.src = "./images/cat.gif";
         document.body.appendChild(img);
-        document.body.style.backgroundColor = "white";
         let resultDiv = document.createElement("div");
         let seconds = (Date.now() - startTime) / 1000;
         let minutes = Math.floor(seconds / 60);
         let extraSeconds = seconds % 60;
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        extraSeconds = extraSeconds < 10 ? parseFloat("0" + extraSeconds) : extraSeconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes.toString();
         extraSeconds = Math.round(extraSeconds);
+        extraSeconds = extraSeconds < 10 ? "0" + extraSeconds : extraSeconds.toString();
         resultDiv.innerHTML = 'It took you ' + minutes + ' Minutes and ' + extraSeconds + ' Seconds to complete the Challenge!';
         document.body.appendChild(resultDiv);
         let resultObj = {
