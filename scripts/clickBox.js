0b531983e6ec6f9fdbd97ff09caafe4ab37a86d1
Clemens Pilz <office@clemenspilz.com>
2023-02-01 18:47:37 +0100

    Optimized styling of cSlides

--- scripts/clickBox.js
+++ scripts/clickBox.js
@@ -13,7 +13,7 @@ export default function setClickBox() {
         clickBox.style.transform = 'translate(-' + x + '%, -' + y + '%)';
         count++;
         counter.innerText = count.toString();
-        if (count >= 35) {
+        if (count >= 19) {
             clickBox.removeEventListener('mouseover', moveClickBox);
             alert('That\'s enough');
             clickBox.parentElement.classList.add("o-50");
